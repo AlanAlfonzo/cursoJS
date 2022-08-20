8fbfa9abb4155e29e8f24617f06be3a3d15e27f5
AlanAlfonzo <111659620+AlanAlfonzo@users.noreply.github.com>
2022-08-20 18:33:45 -0300

    Add files via upload

---
+++ carpeta curso javaScript/desafio1/index.js
@@ -0,0 +1,14 @@
+let Numero = Number(prompt("ingrese el numero a multiplicar"));
+
+for (let i = 1; i <= 10; i++) {
+    let multiplicacion = Numero * i;
+    alert(`${Numero} * ${i} = ${multiplicacion}`);
+    
+}
+alert("gracias por su tiempo");
+
+if (prompt("¿te gusto mi trabajo?, ingrese si o no") ==="si") {
+    
+    alert("¡me alegro!😎");
+}
+else(prompt("bueno chau 😭"));
